a98cb344e6fa9babda528b87ab6dd5aa74ec75a9
Tan Kim Loong <aurelius0523@gmail.com>
2018-05-16 18:27:49 +0800

    added debugging to find out issues

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,23 +1,53 @@
 import { Base64 } from 'js-base64';
 
-const LTDevUsername = 'blf';
-const LTDevPassword = 'blf123';
+const LVDevUsername = 'blf';
+const LVDevPassword = 'blf123';
 
 export const services = Object.freeze([
   {
-    url: 'https://api.github.com/users/github',
-    method: 'GET',
-    requestBody: '',
-    successCriteriaCallback: validIfResponseBodyIsNotNull
-  },
-  {
-    url: 'https://api.github.com/users/github',
-    method: 'GET',
-    requestBody: '',
-    // authorization: Base64.encode(`${LTDevUsername}:${LTDevPassword}`),
-    successCriteriaCallback: validIfResponseBodyIsNotNull
+    url:
+      'https://blis-sl-rd-app.unibanka.lv:9443/LVLifeServices/customer/basic',
+    method: 'POST',
+    requestBody: JSON.stringify({
+      unifiedServiceHeader: {
+        requestId: 'Re527820b-0c0b-4b66-a01d-74a4d72028dc',
+        serviceName: '/customer/basic',
+        userId: 'v20531',
+        xsdBasedRequest: false,
+        responseIsCompressed: false,
+        clientApplication: 'BLF',
+        countryCode: 'LV',
+        language: 'lv',
+        sessionKey: 'A52D79895EA2300D28FE9F649EE43A72'
+      },
+      unifiedServiceBody: {
+        any: [
+          {
+            bankCustomerId: '01951432'
+          }
+        ]
+      }
+    }),
+    authorization: encodeBase64ForLVDev(),
+    successCriteriaCallback: validIfUnifiedServiceBodyIsPresent
   }
+  // ,
+  // {
+  //   url: 'https://api.github.com/users/github',
+  //   method: 'GET',
+  //   requestBody: '',
+  //   // authorization: Base64.encode(`${LTDevUsername}:${LTDevPassword}`),
+  //   successCriteriaCallback: validIfResponseBodyIsNotNull
+  // }
 ]);
+
+function encodeBase64ForLVDev() {
+  return Base64.encode(`${LVDevUsername}:${LVDevPassword}`);
+}
+
+function validIfUnifiedServiceBodyIsPresent(json) {
+  return JSON.stringify(json).includes('unifiedServiceBody');
+}
 
 function validIfResponseBodyIsNotNull(json) {
   return JSON.stringify(json).includes('avatar_url');
--- src/index.js
+++ src/index.js
@@ -2,47 +2,50 @@ import cron from 'node-cron';
 import fetch from 'node-fetch';
 import { services } from './endpoints';
 import fs from 'fs';
+import https from 'https';
 
 let urlToStatus = new Map();
 
-services.map(service => {
-  putUrlInMap(service.url);
-});
-
-cron.schedule('* * * * *', () => {
+const startDate = new Date();
+let id = 0;
+cron.schedule('* * * * * *', () => {
+  urlToStatus.forEach((value, key, map) => {
+    console.log(`${id}: ${key} : ${JSON.stringify(value, null, 2)}`);
+  });
   services.map(service => {
+    putUrlInMap(service.url);
     fetch(service.url, {
       method: service.method,
       headers: {
-        Authorization: 'Basic ' + service.authorization
+        Authorization: 'Basic ' + service.authorization,
+        'Content-Type': 'application/json'
       },
-      body: service.method === 'POST' ? service.requestBody : null
+      body: service.method === 'POST' ? service.requestBody : null,
+      agent: new https.Agent({
+        rejectUnauthorized: false
+      })
     })
-      .then(res => isHttpStatusValid(service.url, res))
-      .then(json => isResponseValid(json, service.successCriteriaCallback))
+      .then(res => isHttpStatusValid(id, service.url, res))
+      .then(json => isResponseValid(id, json, service.successCriteriaCallback))
       .then(isValid => {
+        let status = urlToStatus.get(service.url);
         urlToStatus.set(
           service.url,
-          increaseSuccessOrFailMinutes(
-            isValid,
-            urlToStatus.get(service.url),
-            message
-          )
+          increaseSuccessOrFailMinutes(id, isValid, status)
         );
       })
       .catch(e => {
-        urlToStatus.set(
-          service.url,
-          increaseFailedMinutes(urlToStatus.get(service.url))
-        );
+        console.log(e.message);
+        let status = increaseFailedMinutes(id, urlToStatus.get(service.url));
+        urlToStatus.set(service.url, status);
       });
   });
-
+  id++;
   writeResultToFile(urlToStatus);
 });
 
 function writeResultToFile(urlToStatus) {
-  let stringToWrite = `Date started: ${new Date()}\n\n`;
+  let stringToWrite = `Started at: ${startDate}\n\n`;
 
   urlToStatus.forEach((value, key, map) => {
     stringToWrite += `${key} : ${JSON.stringify(value, null, 2)}\n\n`;
@@ -64,32 +67,44 @@ function putUrlInMap(url) {
   }
 }
 
-function isHttpStatusValid(url, res) {
+function isHttpStatusValid(id, url, res) {
   if (!res.status.toString().match(/^2[0-9][0-9]/)) {
+    console.log(`${id} http status invalid`);
     throw new Error(`Http status returned for ${url} is ${res.status}`);
   } else {
+    console.log(`${id} http status valid`);
     return res.json();
   }
 }
 
-function isResponseValid(json, successCriteriaCallback) {
+function isResponseValid(id, json, successCriteriaCallback) {
+  console.log(`${id} sucessCriteria ${successCriteriaCallback(json)}`);
+
+  if (!successCriteriaCallback) {
+    return true;
+  }
+
   return successCriteriaCallback(json);
 }
 
-function increaseSuccessOrFailMinutes(isValid, status) {
+function increaseSuccessOrFailMinutes(id, isValid, status) {
   if (isValid) {
-    increaseSuccessfulMinutes(status);
+    console.log(`${id} is valid!`);
+    increaseSuccessfulMinutes(id, status);
   } else {
-    increaseFailedMinutes(status);
+    console.log(`${id} is invalid!`);
+    increaseFailedMinutes(id, status);
   }
 }
 
-function increaseSuccessfulMinutes(status) {
+function increaseSuccessfulMinutes(id, status) {
   status.successfulMinutes += 1;
+  console.log(`${id}: increase: ${JSON.stringify(status)}`);
   return status;
 }
 
-function increaseFailedMinutes(status) {
+function increaseFailedMinutes(id, status) {
   status.failedMinutes += 1;
+  console.log(`${id}: decrease: ${JSON.stringify(status)}`);
   return status;
 }
